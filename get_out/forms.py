d4445a154a5d3932d5c1ce9322b9de2225a9526f
Chris Speck <cgspeck@gmail.com>
2013-06-01 14:25:58 +1000

    Adding flora and fauna types to search form

--- get_out/forms.py
+++ get_out/forms.py
@@ -2,3 +2,11 @@ from django import forms
 
 class LocationSearchForm(forms.Form):
     location = forms.CharField(max_length=100)
+    fish = forms.BooleanField(required=False, initial=False)
+    birds = forms.BooleanField(required=False, initial=True)
+    mammals = forms.BooleanField(required=False, initial=True)
+    marsupials = forms.BooleanField(required=False, initial=True)
+    reptiles = forms.BooleanField(required=False, initial=True)
+    plants = forms.BooleanField(required=False, initial=True)
+    trees = forms.BooleanField(required=False, initial=True)
+    other = forms.BooleanField(required=False, initial=True)
