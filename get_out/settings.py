5748fb6901c7dcc95cf5de3b4c56e2e8688d97bc
David Zderic <david.zderic@hitwise.com>
2013-06-01 04:52:51 +0000

    Using collected static properly

--- get_out/settings.py
+++ get_out/settings.py
@@ -38,7 +38,7 @@ MEDIA_URL = '/media/'
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = pkg_resources.resource_filename('get_out', 'static')
+STATIC_ROOT = pkg_resources.resource_filename('get_out', 'collectd_static')
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
@@ -49,6 +49,7 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    pkg_resources.resource_filename('get_out', 'static'),
 )
 
 # List of finder classes that know how to find static files in
