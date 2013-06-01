7b1d6d48cdcbfb0ce259230134543e44fbe200d1
Chris Speck <cgspeck@gmail.com>
2013-06-01 14:01:27 +1000

    Search box on index page Forms class

---
+++ get_out/forms.py
@@ -0,0 +1,4 @@
+from django import forms
+
+class LocationSearchForm(forms.Form):
+    location = forms.CharField(max_length=100)
--- get_out/views.py
+++ get_out/views.py
@@ -1,15 +1,28 @@
 from django.template import RequestContext, loader
 from django.http import HttpResponse
-from django.shortcuts import render_to_response
+from django.shortcuts import render_to_response, render
 
 from atlas_api.location import Location
 
+from get_out import forms
+
 import json
 
-def index(request):
-    template = loader.get_template('templates/index.html')
-    context = RequestContext(request)
-    return HttpResponse(template.render(context))
+def index(request):    
+    form = None
+
+    if request.method == 'POST': # If the form has been submitted...
+        form = forms.LocationSearchForm(request.POST) # A form bound to the POST data
+        if form.is_valid(): # All validation rules pass
+            # Process the data in form.cleaned_data
+            # ...
+            return HttpResponse('FORM SUBMISSION WORKED!') # Redirect after POST
+    else:
+        form = forms.LocationSearchForm() # An unbound form
+
+    return render(request, 'templates/index.html', {
+        'form': form
+    })
 
 def list_view(request):
     return render_to_response('templates/list.html', {'things': [1,2,3]}, context_instance=RequestContext(request))
