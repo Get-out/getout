aa62689c9cb2a13c31a9eea1b69071b348afec14
Chris Speck <cgspeck@gmail.com>
2013-06-01 16:08:47 +1000

    Switching check boxes for sexy native sliders

--- get_out/forms.py
+++ get_out/forms.py
@@ -2,11 +2,3 @@ from django import forms
 
 class LocationSearchForm(forms.Form):
     location = forms.CharField(max_length=100, initial="Wilsons Promontory")
-    fish = forms.BooleanField(required=False, initial=False)
-    birds = forms.BooleanField(required=False, initial=True)
-    mammals = forms.BooleanField(required=False, initial=True)
-    marsupials = forms.BooleanField(required=False, initial=True)
-    reptiles = forms.BooleanField(required=False, initial=True)
-    plants = forms.BooleanField(required=False, initial=True)
-    trees = forms.BooleanField(required=False, initial=True)
-    other = forms.BooleanField(required=False, initial=True)
