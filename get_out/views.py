d98e3a83137cf70a92074bcd05a095fc32a6a094
Chris Speck <cgspeck@gmail.com>
2013-06-01 19:04:44 +1000

    Unhardcoding the weighting and adding defaults to the form

--- get_out/views.py
+++ get_out/views.py
@@ -10,22 +10,26 @@ import json
 
 def index(request):    
     form = forms.LocationSearchForm() 
-    things = [({'image':'/static/img/{0}.png'.format(x), 'description':'{0}'.format(x)}) for x in ("birds", "fish", "other_fauna","plants", "trees")]
+    defaults = {"bird":5, "fish":0, "land_animal":5,"plant":5, "tree":5}
+    things = [{'image':'/static/img/{0}.png'.format(x), 'description':'{0}'.format(x), 'default':y} for x, y in defaults.items()]
     return render(request, 'templates/index.html', {
         'form': form,
         'things':things
     })
 
 def list_view(request):
-    location = request.GET.get('location', 'wilsons promontory')
-    # TODO: un-hardcode
-    return render_to_response('templates/list.html', {'things': Location(location).ranked_species(10, {
-        'fish': 0,
-        'bird': 5,
-        'plant': 5,
-        'tree': 5,
-        'other': 5,
-    })}, context_instance=RequestContext(request))
+    location = None
+    species_weight = None
+    #retrieve the location    
+    location = request.REQUEST.get('location', 'wilsons promontory')
+    #and the weights
+    species_weight = {x : int(request.REQUEST.get(x, 5)) for x in ("bird", "plant", "tree")}    
+    #don't include fish unless the user really wants it
+    species_weight['fish'] = int(request.REQUEST.get('fish', 0))
+    #land_animal maps to other
+    species_weight['other'] = int(request.REQUEST.get('land_animal', 5))
+    return render_to_response('templates/list.html', {'things': Location(location).ranked_species(10, species_weight)}, 
+        context_instance=RequestContext(request))
 
 def species(request):
 		name = request.GET.get("name")
